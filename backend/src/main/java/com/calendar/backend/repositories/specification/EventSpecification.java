6a38088caf185892aeba5d8b04a87700d2c79a23
Artem <ma.ua@outlook.com>
2025-02-16 21:34:47 +0200

    Small rename

--- backend/src/main/java/com/calendar/backend/repositories/specification/EventSpecification.java
+++ backend/src/main/java/com/calendar/backend/repositories/specification/EventSpecification.java
@@ -14,33 +14,33 @@ import java.util.Map;
 
 public class EventSpecification {
     public static Specification<Event> filterEvents(Map<String, Object> filters) {
-        return (Root<Event> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+        return (Root<Event> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
             if (filters.containsKey("search")) {
                 String search = "%" + filters.get("search").toString().toLowerCase() + "%";
-                predicates.add(cb.like(cb.lower(root.get("name")), search));
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), search));
             }
 
             if (filters.containsKey("startDate")) {
                 LocalDateTime startDate = LocalDateTime.parse(filters.get("startDate").toString());
-                predicates.add(cb.greaterThanOrEqualTo(root.get("startDate"), startDate));
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("startDate"), startDate));
             }
 
             if (filters.containsKey("endDate")) {
                 LocalDateTime endDate = LocalDateTime.parse(filters.get("endDate").toString());
-                predicates.add(cb.lessThanOrEqualTo(root.get("endDate"), endDate));
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("endDate"), endDate));
             }
 
             if (filters.containsKey("isPast")) {
                 boolean isPast = Boolean.parseBoolean(filters.get("isPast").toString());
                 if (isPast) {
-                    predicates.add(cb.lessThan(root.get("endDate"), LocalDateTime.now()));
+                    predicates.add(criteriaBuilder.lessThan(root.get("endDate"), LocalDateTime.now()));
                 } else {
-                    predicates.add(cb.greaterThanOrEqualTo(root.get("startDate"), LocalDateTime.now()));
+                    predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("startDate"), LocalDateTime.now()));
                 }
             }
-            return cb.and(predicates.toArray(new Predicate[0]));
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
 }
--- backend/src/main/java/com/calendar/backend/repositories/specification/TaskSpecification.java
+++ backend/src/main/java/com/calendar/backend/repositories/specification/TaskSpecification.java
@@ -14,30 +14,30 @@ import java.util.Map;
 
 public class TaskSpecification {
     public static Specification<Task> filterTasks(Map<String, Object> filters) {
-        return (Root<Task> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+        return (Root<Task> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
             if (filters.containsKey("name")) {
                 String name = (String) filters.get("name");
-                predicates.add(cb.like(cb.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
             }
             if (filters.containsKey("deadline")) {
                 LocalDateTime deadline = (LocalDateTime) filters.get("deadline");
-                predicates.add(cb.equal(root.get("deadline"), deadline));
+                predicates.add(criteriaBuilder.equal(root.get("deadline"), deadline));
             }
             if (filters.containsKey("is_done")) {
                 Boolean isDone = (Boolean) filters.get("is_done");
-                predicates.add(cb.equal(root.get("event").get("isDone"), isDone));
+                predicates.add(criteriaBuilder.equal(root.get("event").get("isDone"), isDone));
             }
             if (filters.containsKey("is_past")) {
                 Boolean isPast = (Boolean) filters.get("is_past");
                 if (isPast) {
-                    predicates.add(cb.lessThan(root.get("deadline"), LocalDateTime.now()));
+                    predicates.add(criteriaBuilder.lessThan(root.get("deadline"), LocalDateTime.now()));
                 } else {
-                    predicates.add(cb.greaterThanOrEqualTo(root.get("deadline"), LocalDateTime.now()));
+                    predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("deadline"), LocalDateTime.now()));
                 }
             }
-            return cb.and(predicates.toArray(new Predicate[0]));
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
 }
