d623ff5142329588e192bc48ce01b308c0687d0c
alyakravets <alyakravets2008@gmail.com>
2025-03-26 18:26:13 +0200

    create  exemple calendar

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "../../css/profile.css"
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PersonIcon from '@mui/icons-material/Person';
 import SearchIcon from '@mui/icons-material/Search';
 import CreateIcon from '@mui/icons-material/Create';
 
@@ -14,7 +14,7 @@ const Profile = () => {
             <div className="profile-information">
                 <div className="user-photo-name">
                     <div className="user-photo">
-                        <AccountCircleIcon className="user-photo-icon" />
+                        <PersonIcon className="user-photo-icon" sx={{ fontSize: 250 }} />
                         <button className="edit-userPhoto"><CreateIcon></CreateIcon></button>
                     </div>
                     <div className="user-name">
@@ -58,39 +58,81 @@ const Profile = () => {
                 </table>
                 <button className="user-information-edit">edit</button>
             </div>
-            <div className="profile-calendar">
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-                <div className="calendar-elem"></div>
-            </div>
+
+            <table className="profile-calendar">
+                <thead>
+                    <tr>
+                        <td colSpan={7}>
+                            <p>March</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Monday</td>
+                        <td>Tuesday</td>
+                        <td>Wednesday</td>
+                        <td>Thursday</td>
+                        <td>Friday</td>
+                        <td>Saturday</td>
+                        <td>Sunday</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                        <td>8</td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>10</td>
+                        <td>11</td>
+                        <td>12</td>
+                        <td>13</td>
+                        <td>14</td>
+                        <td>15</td>
+                    </tr>
+                    <tr>
+                        <td>16</td>
+                        <td>17</td>
+                        <td>18</td>
+                        <td>19</td>
+                        <td>20</td>
+                        <td>21</td>
+                        <td>22</td>
+                    </tr>
+                    <tr>
+                        <td>23</td>
+                        <td>24</td>
+                        <td>25</td>
+                        <td className="active-date">26</td>
+                        <td>27</td>
+                        <td>28</td>
+                        <td>29</td>
+                    </tr>
+                    <tr>
+                        <td>30</td>
+                        <td>31</td>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
