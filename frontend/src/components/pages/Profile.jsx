583d3c7d65fe7ddabb01bfe420161c76cb96d1c8
alyakravets <alyakravets2008@gmail.com>
2025-05-02 18:19:04 +0300

    new

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -1,64 +1,65 @@
 import React from "react"
 import "../../css/profile.css"
 import PersonIcon from '@mui/icons-material/Person';
-import SearchIcon from '@mui/icons-material/Search';
-import CreateIcon from '@mui/icons-material/Create';
+import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
 
 const Profile = () => {
     return (
-        <div className="profile-container">
-            <div className="profile-search">
-                <input type="text" placeholder="Пошук користувачів" />
-                <SearchIcon className="search-icon" />
-            </div>
-            <div className="profile-information">
-                <div className="user-photo-name">
-                    <div className="user-photo">
-                        <PersonIcon className="user-photo-icon" sx={{ fontSize: 250 }} />
-                        <button className="edit-userPhoto"><CreateIcon></CreateIcon></button>
-                    </div>
-                    <div className="user-name">
-                        <h2>Sophia Miller</h2>
-                        <CreateIcon className="edit-userName"></CreateIcon>
-                    </div>
-                    <div className="user-description">
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit enim quibusdam similique, molestiae cupiditate eius dolores repudiandae, voluptatem dolor culpa blanditiis ab obcaecati porro architecto dignissimos sunt placeat alias atque?</p>
-                        <CreateIcon className="edit-userDescription"></CreateIcon>
-                    </div>
-                </div>
+        <Box className="profile-container">
+            <Box className="profile-information">
+                <Box className="user-photo">
+                    <PersonIcon className="user-photo-icon" sx={{ fontSize: 250 }} />
+                </Box>
+                <Box className="user-name">
+                    <Typography variant="h4" mb={2}>Sophia Miller</Typography>
+                </Box>
                 <table className="user-information">
                     <tr>
-                        <td>birthday:</td>
                         <td>
-                            <p>14.06.2011</p>
+                            <Typography variant="p">birthday:</Typography>
+                        </td>
+                        <td>
+                            <Typography variant="p">14.06.2011</Typography>
                         </td>
                     </tr>
                     <tr>
-                        <td>email:</td>
                         <td>
-                            <p>user00001@gmail.com</p>
+                            <Typography variant="p">email:</Typography>
+                        </td>
+                        <td>
+                            <Typography variant="p">user00001@gmail.com</Typography>
                         </td>
                     </tr>
                     <tr>
-                        <td>number:</td>
                         <td>
-                            <p>+3806666666</p>
+                            <Typography variant="p">number:</Typography>
+                        </td>
+                        <td>
+                            <Typography variant="p">+3806666666</Typography>
                         </td>
                     </tr>
                     <tr>
-                        <td>adress:</td>
                         <td>
-                            <p>street 5A</p>
+                            <Typography variant="p">adress:</Typography>
+                        </td>
+                        <td>
+                            <Typography variant="p">street 5A</Typography>
                         </td>
                     </tr>
                     <tr>
-                        <td>class:</td>
-                        <td><p>11B</p></td>
+                        <td>
+                            <Typography variant="p">class:</Typography>
+                        </td>
+                        <td>
+                            <Typography variant="p" >11B</Typography>
+                        </td>
                     </tr>
                 </table>
-                <button className="user-information-edit">edit</button>
-            </div>
-
+                <Box className="user-description">
+                    <Typography variant="p" mt={2}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit enim quibusdam similique, molestiae cupiditate eius dolores repudiandae, voluptatem dolor culpa blanditiis ab obcaecati porro architecto dignissimos sunt placeat alias atque?</Typography>
+                </Box>
+            </Box>
             <table className="profile-calendar">
                 <thead>
                     <tr>
@@ -133,7 +134,7 @@ const Profile = () => {
                     </tr>
                 </tbody>
             </table>
-        </div>
+        </Box>
     )
 }
 
