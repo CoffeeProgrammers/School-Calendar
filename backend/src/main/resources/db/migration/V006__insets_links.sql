6dcd973002ba7883f6df54fab4309c0edb655ab0
Artem <ma.ua@outlook.com>
2025-05-05 21:18:37 +0300

    Add some inserts

---
+++ backend/src/main/resources/db/migration/V006__insets_links.sql
@@ -0,0 +1,15 @@
+INSERT INTO "users_events" ("user_id", "event_id") VALUES
+                                                       (1, 1),
+                                                       (2, 1),
+                                                       (1, 2),
+                                                       (2, 2),
+                                                       (1, 3),
+                                                       (2, 3),
+                                                       (5, 1),
+                                                       (3, 2);
+
+
+INSERT INTO "task_assignments" ("user_id", "task_id", "is_done") VALUES
+                                                                     (1, 1, false),
+                                                                     (4, 3, false),
+                                                                     (2, 2, true);
