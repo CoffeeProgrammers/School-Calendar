4d9a3295a59708b8df0a29a122e12abaef63cf80
alyakravets <alyakravets2008@gmail.com>
2025-05-05 18:29:41 +0300

    start work with notifications page

---
+++ frontend/src/components/pages/notifications/NotificationsPage.jsx
@@ -0,0 +1,14 @@
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+const NotificationsPage = () => {
+    return (
+        <Box>
+            <Tabs
+                textColor='primary'>
+
+            </Tabs>
+        </Box>
+    )
+}
