3ce7dd3867f3083cc045c4655f7a07d6af2e6f8a
alyakravets <alyakravets2008@gmail.com>
2025-05-05 18:57:46 +0300

    work with notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from "./components/pages/Main";
 import Page from "./components/layouts/Page";
-import {ThemeProvider} from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import theme from "./assets/theme"
 import Users from "./components/pages/user/Users";
 import Tasks from "./components/pages/task/Tasks";
@@ -10,6 +10,7 @@ import Login from "./components/pages/login/Login";
 import Events from "./components/pages/event/Events";
 import UserPage from "./components/pages/user/UserPage";
 import MyProfile from "./components/pages/user/MyProfile";
+import NotificationsPage from './components/pages/notifications/NotificationsPage';
 
 function App() {
     return (
@@ -17,14 +18,15 @@ function App() {
             <ThemeProvider theme={theme}>
                 <Page>
                     <Routes>
-                        <Route path={""} element={<Main/>}/>
-                        <Route path={"/login"} element={<Login/>}/>
-                        <Route path={"/events"} element={<Events/>}/>
-                        <Route path={"/users"} element={<Users/>}/>
-                        <Route path={"/tasks"} element={<Tasks/>}/>
-                        <Route path={"/events/:id"} element={<Event/>}/>
-                        <Route path={'/profile'} element={<MyProfile/>} />
-                        <Route path={"/users/:id"} element={<UserPage/>}/>
+                        <Route path={""} element={<Main />} />
+                        <Route path={"/login"} element={<Login />} />
+                        <Route path={"/events"} element={<Events />} />
+                        <Route path={"/users"} element={<Users />} />
+                        <Route path={"/tasks"} element={<Tasks />} />
+                        <Route path={"/events/:id"} element={<Event />} />
+                        <Route path={'/profile'} element={<MyProfile />} />
+                        <Route path={"/users/:id"} element={<UserPage />} />
+                        <Route path={"/notifications"} element={<NotificationsPage />} />
                     </Routes>
                 </Page>
             </ThemeProvider>
--- frontend/src/components/pages/notifications/NotificationsPage.jsx
+++ frontend/src/components/pages/notifications/NotificationsPage.jsx
@@ -4,11 +4,16 @@ import Tab from '@mui/material/Tab';
 
 const NotificationsPage = () => {
     return (
-        <Box>
+        <Box sx={{ width: "50%", border: "1px solid black", padding: "10px" }}>
             <Tabs
-                textColor='primary'>
-
+                sx={{ textColor: "#333" }}
+                textColor="#333"
+                indicatorColor="secondary">
+                <Tab value='notification' label="Notification" />
+                <Tab value='invitations' label="Invitations" />
             </Tabs>
         </Box>
     )
 }
+
+export default NotificationsPage
