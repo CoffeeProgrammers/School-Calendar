1de7ccc822eecb4bb7c2eda5045f823fff9db444
Artem <ma.ua@outlook.com>
2025-05-18 22:14:13 +0300

    Small changes

--- backend/src/main/java/com/calendar/backend/repositories/TaskRepository.java
+++ backend/src/main/java/com/calendar/backend/repositories/TaskRepository.java
@@ -3,7 +3,6 @@ package com.calendar.backend.repositories;
 import com.calendar.backend.models.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -12,11 +11,11 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
-    Page<Task> findAllByEvent_Id(Long eventId, Pageable pageable, Specification<Task> specification);
+    Page<Task> findAllByEvent_Id(Long eventId, Pageable pageable);
 
     List<Task> findAllByEvent_Id(Long eventId);
 
-    Page<Task> findAllByCreator_IdAndEventIsNull(long creator_id, Pageable pageable, Specification<Task> specification);
+    Page<Task> findAllByCreator_IdAndEventIsNull(long creator_id, Pageable pageable);
 
     @Query(value =
             "SELECT count(t.id)" +
