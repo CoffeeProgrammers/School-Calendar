c7c4e48ee1ce0827727d9af6e4c2ed4887f39b9d
Artem <ma.ua@outlook.com>
2025-05-18 16:31:55 +0300

    Add a new method

--- backend/src/test/java/com/calendar/backend/controllers/CommentControllerTest.java
+++ backend/src/test/java/com/calendar/backend/controllers/CommentControllerTest.java
@@ -15,9 +15,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -36,14 +36,16 @@ class CommentControllerTest{
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private RefreshTokenServiceImpl refreshTokenService;
 
-    @MockBean
+    @MockitoBean
     private CommentService commentService;
-    @MockBean
+
+    @MockitoBean
     private JwtUtils jwtUtils;
-    @MockBean
+
+    @MockitoBean
     private UserServiceImpl userService;
 
     private CommentRequest commentRequest;
--- backend/src/test/java/com/calendar/backend/controllers/EventControllerTest.java
+++ backend/src/test/java/com/calendar/backend/controllers/EventControllerTest.java
@@ -21,9 +21,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -44,19 +44,19 @@ class EventControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private EventServiceImpl eventService;
 
-    @MockBean
+    @MockitoBean
     private UserServiceImpl userService;
 
-    @MockBean
+    @MockitoBean
     private JwtUtils jwtUtils;
 
-    @MockBean
+    @MockitoBean
     private TaskServicesImpl taskService;
 
-    @MockBean
+    @MockitoBean
     private RefreshTokenServiceImpl refreshTokenService;
 
     private EventCreateRequest eventCreateRequest;
